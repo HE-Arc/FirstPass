a48f909e3c3ed826605b45d6086059f89d9d0869
Xavier Stouder <xavier@stouder.io>
2023-02-26 21:16:41 +0100

    feat: add login route

--- api/firstpassapp/urls.py
+++ api/firstpassapp/urls.py
@@ -3,9 +3,10 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register("accounts", views.AccountViewSet, basename="account")
-router.register("users", views.UserViewSet, basename="user")
+router.register('accounts', views.AccountViewSet, basename='account')
+router.register('users', views.UserViewSet, basename='user')
 
 urlpatterns = [
+    path('auth/login/', views.login_view, name='auth-login'),
     path('', include(router.urls))
 ]
