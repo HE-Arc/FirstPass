288f01aa768d70c52ca2c63b6bc0c2119d7b878e
Xavier Stouder <xavier@stouder.io>
2023-02-26 15:12:35 +0100

    fix: remove custom loggin in production

--- api/firstpass/settings/production.py
+++ api/firstpass/settings/production.py
@@ -34,33 +34,3 @@ DATABASES = {
 }
 
 SECRET_KEY = db_datas['password']
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "root": {"level": "INFO", "handlers": ["file"]},
-    "handlers": {
-        "file": {
-            "level": "INFO",
-            "class": "logging.FileHandler",
-            "filename": "/var/log/django.log",
-            "formatter": "app",
-        },
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["file"],
-            "level": "INFO",
-            "propagate": True
-        },
-    },
-    "formatters": {
-        "app": {
-            "format": (
-                u"%(asctime)s [%(levelname)-8s] "
-                "(%(module)s.%(funcName)s) %(message)s"
-            ),
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-    },
-}
