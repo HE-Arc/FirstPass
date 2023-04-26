23560262e42381e8a54c1947c2b351018bcf1568
Xavier Stouder <xavier@stouder.io>
2023-04-26 20:54:23 +0200

    fix: try to fix

--- api/firstpassapp/urls.py
+++ api/firstpassapp/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('accounts', views.AccountViewSet, basename='account')
 router.register('users', views.UserViewSet, basename='user')
-#router.register('vaults', views.VaultViewSet, basename='vault')
+router.register('vaults', views.VaultViewSet, basename='vault')
 router.register('invitations', views.InvitationViewSet, basename='invitation')
 
 
