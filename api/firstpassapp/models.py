beb103d861f75c55830907684379d876eb40dd73
Xavier Stouder <xavier@stouder.io>
2023-04-30 22:12:36 +0200

    fix: add str representations for models

--- api/firstpassapp/models.py
+++ api/firstpassapp/models.py
@@ -33,6 +33,9 @@ class Vault(models.Model):
     image_path = models.CharField(max_length=255)
     users = models.ManyToManyField(Account, through='AccountVaultAccess')
 
+    def __str__(self):
+        return 'Vault[id={}, name={}, image_path={}]'.format(self.id, self.name, self.image_path)
+
 
 class Pair(models.Model):
     application = models.TextField()
@@ -40,14 +43,23 @@ class Pair(models.Model):
     password = models.TextField()
     vault = models.ForeignKey(Vault, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return 'Pair[id={}, application={}, username={}, password={}]'.format(self.id, self.application, self.username, self.password)
+
 
 class Invitation(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     vault = models.ForeignKey(Vault, on_delete=models.CASCADE)
     access_level = models.CharField(max_length=1, choices=ACCESS_LEVEL)
 
+    def __str__(self):
+        return 'Invitation[id={}, account={}, vault={}, access_level={}]'.format(self.id, self.account, self.vault, self.access_level)
+
 
 class AccountVaultAccess(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     vault = models.ForeignKey(Vault, on_delete=models.CASCADE)
     access_level = models.CharField(max_length=1, choices=ACCESS_LEVEL)
+
+    def __str__(self):
+        return 'AccountVaultAccess[id={}, account={}, vault={}, access_level={}]'.format(self.id, self.account, self.vault, self.access_level)
